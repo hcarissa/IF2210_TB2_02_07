318e43551a2ab97bf5800a1e9405b4c5e282597e
ravaattar <rava.naufal2014@gmail.com>
2022-04-13 15:59:50 +0700

    added raw BoardCard

---
+++ src/main/java/com/aetherwars/model/BoardCard.java
@@ -0,0 +1,36 @@
+package com.aetherwars.model;
+
+public class BoardCard {
+    private int position;
+    private int currExp;
+    private int needsExp;
+    private int currLvl;
+
+    public BoardCard(int position) {
+        this.position = position;
+        this.currExp = 0;
+        this.needsExp = 1;
+        this.currLvl = 1;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+    public int getCurrExp() {
+        return this.currExp;
+    }
+    public int getNeedsExp() {
+        return this.needsExp;
+    }
+    public int getCurrLvl() {
+        return this.currLvl;
+    }
+
+    public earnExp(int expEarned) {
+        this.currExp += expEarned;
+        if (getCurrExp() >= getNeedsExp()) {
+            this.currExp -= getNeedsExp();
+            this.currLvl++;
+        }
+    }
+}
