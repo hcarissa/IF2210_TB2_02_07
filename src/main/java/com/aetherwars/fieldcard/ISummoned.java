be2c5ba817e2cd47cc15d7e41d6881393660b50d
ravaattar <rava.naufal2014@gmail.com>
2022-04-26 20:44:15 +0700

    update SummonedCharacter implements ISummoned

--- src/main/java/com/aetherwars/fieldcard/ISummoned.java
+++ src/main/java/com/aetherwars/fieldcard/ISummoned.java
@@ -1,5 +1,16 @@
+package com.aetherwars.fieldcard;
+import com.aetherwars.card.*;
 import java.util.List;
-
 interface ISummoned {
-    
+    // getter
+    public int getPosition();
+    public List<SpellCard> getActiveSpells();
+    public int getExp();
+    public int getNeedsExp();
+    public int getTotalExp();
+    public int getLvl();
+
+    // operation
+    public void earnExp(int exp);
+    public void addSpell(SpellCard spell);
 }
